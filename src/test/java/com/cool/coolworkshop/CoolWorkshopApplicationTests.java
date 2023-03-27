cc851f0ca5d2fdbd74052398b71318cf5901ca5c
Becky Yeung <becky.yeung@oocl.com>
2023-03-27 10:37:18 +0800

    Initial commit

---
+++ src/test/java/com/cool/coolworkshop/CoolWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cool.coolworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
